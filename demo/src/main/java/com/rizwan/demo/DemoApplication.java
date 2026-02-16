20f0cb0f06e9d4576e9adb8aa2fb026babc6c750
Rizwan Vahora <vahorarizwan09@gmail.com>
2026-02-16 16:35:23 +0530

    d

--- demo/src/main/java/com/rizwan/demo/DemoApplication.java
+++ demo/src/main/java/com/rizwan/demo/DemoApplication.java
@@ -2,11 +2,16 @@ package com.rizwan.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv=Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));		 
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello World");
 	}
