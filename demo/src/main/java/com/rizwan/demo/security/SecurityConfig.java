975e68a98dace723b431f65ab0d1e8a40f55dce8
Rizwan Vahora <vahorarizwan09@gmail.com>
2026-02-16 18:31:16 +0530

    d

--- demo/src/main/java/com/rizwan/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/rizwan/demo/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://e-commerce-1-kb1a.onrender.com"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
